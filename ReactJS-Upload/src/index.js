f1ca918856283098dd871d29c6baf5683b92cd56
QrQe-Mgmt <qrqemgmt@gmail.com>
2025-07-26 22:49:20 -0700

    Added gh-pages to deploy React app

--- ReactJS-Upload/src/index.js
+++ ReactJS-Upload/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import { createRoot } from 'react-dom/client'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import { createRoot } from 'react-dom/client'; 
 
 
 ReactDOM.render(
