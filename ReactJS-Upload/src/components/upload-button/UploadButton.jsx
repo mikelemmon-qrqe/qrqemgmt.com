be45399cd01fd55de6f8e93b0fd514d7278561a1
QrQe-Mgmt <qrqemgmt@gmail.com>
2025-07-26 20:28:10 -0700

    Updated index.html

---
+++ ReactJS-Upload/src/components/upload-button/UploadButton.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './upload-button.css'
+import PropTypes from 'prop-types'
+
+const UploadButton = props => {
+
+    return(
+        <div className="uploadButton" onClick={props.onClick}> Upload Button </div>
+    );
+
+}
+
+UploadButton.propTypes = {
+    onClick: PropTypes.func
+}
+
+export default UploadButton;    
---
+++ ReactJS-Upload/src/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAmgVdJoLNl1muRcaJoCfPnJD_jXkKaXbQ",
+  authDomain: "qrqe-mgmt---video-submission.firebaseapp.com",
+  projectId: "qrqe-mgmt---video-submission",
+  storageBucket: "qrqe-mgmt---video-submission.firebasestorage.app",
+  messagingSenderId: "908001990389",
+  appId: "1:908001990389:web:0cb1d95059978bae95d7bc",
+  measurementId: "G-D3WVQ5YXLP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const storage = getStorage(app);
+export const db = getFirestore(app);
