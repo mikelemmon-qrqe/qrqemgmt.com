f140eb74f26c3c40939c7e25eabc16c7511718f0
QrQe-Mgmt <qrqemgmt@gmail.com>
2025-07-27 15:10:09 -0700

    Updated jest.config.js file

--- ReactJS-Upload/jest.config.js
+++ ReactJS-Upload/jest.config.js
@@ -1,9 +1,6 @@
-// jest.config.js
-module.exports = {
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['ReactJS-Upload/src/setupTests.js'], // Optional: for @testing-library/jest-dom
-    moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // Optional: for CSS Modules
-    },
-    // Add other Jest configurations as needed
+/** @type {import('jest').Config} */
+const config = {
+  verbose: true,
 };
+
+module.exports = config;
