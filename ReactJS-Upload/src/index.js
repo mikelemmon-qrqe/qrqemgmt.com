93f1bcefe6fdee352f598882105c9e9c9f726741
QrQe-Mgmt <qrqemgmt@gmail.com>
2025-07-27 19:47:46 -0700

    Added load env variables line to index.js

--- ReactJS-Upload/src/index.js
+++ ReactJS-Upload/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import { createRoot } from 'react-dom/client'; 
-
+require('dotenv').config(); // Load environment variables from .env file
 
 ReactDOM.render(
   <React.StrictMode>
