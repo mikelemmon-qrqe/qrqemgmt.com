b6126f802b50134927c969c460f6dc5a50c67ac6
QrQe-Mgmt <qrqemgmt@gmail.com>
2025-08-08 15:52:25 -0700

    Update

--- ReactJS-Upload/src/index.js
+++ ReactJS-Upload/src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { createRoot } from 'react-dom/client'; 
-// require('dotenv').config(); // Load environment variables from .env file
+
 
 ReactDOM.render(
   <React.StrictMode>
